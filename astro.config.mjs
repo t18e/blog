6f03bf16e7d78552abda806570f3b8e4cbad8324
t18e <tiwaadejuyigbe@gmail.com>
2024-12-08 02:22:04 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  }
 });
